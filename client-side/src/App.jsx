468619ebc26512f8516ba27bfe94d79081a8aba3
Anurag <coleridgeprotube@gmail.com>
2025-01-08 00:24:21 +0530

    modify app.jsx and main.jsx as needed

--- client-side/src/App.jsx
+++ client-side/src/App.jsx
@@ -1,34 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LandingPage from "./pages/LandingPage/LandingPage";
+import LoginPage from "./pages/LoginPage/LoginPage";
+import VideoLists from './pages/VideoLists/VideoLists';
+import Education from './pages/Education/Education';
+import NoticeBoard from "./pages/Noticeboard/Noticeboard"
+import Leaderboard from './pages/Leaderboard/Leaderboard';
+import DashBoard from './pages/DashBoard/dashboard';
+import TeamMember from "./pages/Team/Team"
+import Signup from "./pages/SignUp/signUp"
+import VerifyEmail from "./pages/SignUp/verifyEmail"
+import VerifyCfID from './pages/SignUp/verifyCfID';
+import ContactUs from './pages/ContactUs/ContactUs';
+import ProtectedRoute from './components/auth/ProtectRoute';
+import Navbar from './components/NavBar/NavBar';
+import { AuthWrapper } from './lib/AuthWrapper';
+import ForgotPassword from './pages/SignUp/ForgetPassword'; import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <BrowserRouter>
+      <AuthWrapper>
+        <Navbar />
+        <Routes>
+          <Route path={"/"} element={<LandingPage />} />
+          <Route path={"/login"} element={<LoginPage />} />
+          <Route path={"/signUp"} element={<Signup />} />
+          {/* Route for Forgot Password */}
+          <Route path={"/forgot-password"} element={<ForgotPassword />} />
+
+          <Route path={"/verify-email"} element={<VerifyEmail />} />
+          <Route path={"/verify-cf-id"} element={<VerifyCfID />} />
+          <Route path={"/education/videos"} element={<VideoLists />} />
+          <Route path={"/education"} element={<Education />} />
+          <Route path={"/notice-board"} element={<NoticeBoard />} />
+          <Route path={"/dashBoard"} element={<ProtectedRoute>
+            <DashBoard />
+          </ProtectedRoute>} />
+          <Route path={"/leader-board"} element={<Leaderboard />} />
+          <Route path={"/team-member"} element={<TeamMember />} />
+          <Route path={"/contact-us"} element={<ContactUs />} />
+          <Route path={"/get-codeforces-profile/:id"} element={<ProtectedRoute>
+            <DashBoard />
+          </ProtectedRoute>} />
+        </Routes>
+      </AuthWrapper>
+    </BrowserRouter >
   )
 }
 
--- client-side/src/main.jsx
+++ client-side/src/main.jsx
@@ -3,8 +3,16 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import { PersistGate } from 'redux-persist/integration/react';
+import { Provider } from 'react-redux';
+import { store, persistor } from './redux/store';
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <StrictMode>
+        <App />
+      </StrictMode>,
+    </PersistGate>
+  </Provider>
 )
