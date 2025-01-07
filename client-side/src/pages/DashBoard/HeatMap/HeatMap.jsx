f407764e354a6129014a3dfbc25d47c779d17cf6
rishisulakhe <rishiprasadsulakhe@gmail.com>
2025-01-07 13:31:38 +0530

    Updated HeatMap

--- client-side/src/pages/DashBoard/HeatMap/HeatMap.jsx
+++ client-side/src/pages/DashBoard/HeatMap/HeatMap.jsx
@@ -134,16 +134,16 @@ const Heatmap = ({ handle }) => {
   };
 
   return (
-    <div className="w-full h-[400px] mt-4 mb-20 rounded-lg p-4  lg:h-[400px] md:h-[300px] sm:h-[250px] xs:h-[200px] xxs:h-[150px]">
-      <h4 className="text-3xl md:text-5xl font-bold text-[#05CBDC] text-center mb-6">
-        Heatmap
-      </h4>
-      <div className="flex justify-center">
-      {error && <p style={{ color: "red" }}>{error}</p>}
-      <svg id="heatmap"></svg>
+      <div className="w-full h-auto mt-4 mb-20 rounded-lg p-4">
+        <h4 className="text-3xl md:text-5xl font-bold text-[#05CBDC] text-center mb-6">
+          Heatmap
+        </h4>
+        <div className="flex justify-center">
+          {error && <p style={{ color: "red" }}>{error}</p>}
+          <svg id="heatmap" style={{ width: "100%", height: "auto" }}></svg>
+        </div>
       </div>
-    </div>
-  );
+    );
 };
 
 export default Heatmap;
